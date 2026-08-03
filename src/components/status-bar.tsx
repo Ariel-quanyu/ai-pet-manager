e13c2209ff8b7c3fcbd1c56b5d59d730c80b3186
quanyu-qian <qqia0010@student.monash.edu>
2026-08-03 17:10:30 +0800

    Fix duplicate status bar rendering

--- src/components/status-bar.tsx
+++ src/components/status-bar.tsx
@@ -1,3 +1,6 @@
-import { Text, View } from '@tarojs/components'
+import { View } from '@tarojs/components'
 import './status-bar.scss'
-export function StatusBar({ light=false }: { light?: boolean }) { return <View className={`status safe-top ${light?'status--light':''}`}><Text>12:00</Text><View className='status__signals'><Text>▮▮▮</Text><Text>⌁</Text><Text>▱</Text></View></View> }
+
+export function StatusBar() {
+  return <View className="status safe-top" />
+}
